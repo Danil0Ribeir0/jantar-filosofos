38572e3a48881c9d6b265fb1b0b2f280c2edd931
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2026-05-02 17:08:46 -0300

    refactor: remove legenda

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Filosofo[] filosofos = new Filosofo[numFilosofos];
 
         System.out.println("Iniciando o Jantar dos Filósofos...\n");
-        System.out.println("Legenda: \u001B[34m[PENSANDO]\u001B[0m | \u001B[33m[TENTANDO]\u001B[0m | \u001B[32m[COMENDO]\u001B[0m | \u001B[36m[DEVOLVENDO]\u001B[0m | \u001B[31m[MORTO]\u001B[0m\n");
 
         for (int i = 0; i < numFilosofos; i++) {
             filosofos[i] = new Filosofo(i, mesa);
